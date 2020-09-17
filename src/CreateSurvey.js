6b4e9f7b318f9c21c21604371fde80b21452fe11
Anushree P M <65195869+Anushreepm@users.noreply.github.com>
2020-09-17 17:25:20 +0530

    first commit

---
+++ src/CreateSurvey.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PetsSharpIcon from "@material-ui/icons/PetsSharp";
+
+const CreateSurvey = (props) => {
+  return (
+    <div>
+      <h1 style={{ color: "green" }}>
+        {" "}
+        <PetsSharpIcon /> Survey Tiger
+      </h1>
+    </div>
+  );
+};
+export default CreateSurvey;
